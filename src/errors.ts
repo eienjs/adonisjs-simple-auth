9ebf0df8fcb73383d62fe300f1d571c6eb94114e
Fernando Isidro L <luffynando@gmail.com>
2025-05-08 15:13:14 -0600

    style: :rotating_light: resolve issues reported by prettier

--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,6 @@
 import { Exception } from '@adonisjs/core/exceptions';
 import { type HttpContext } from '@adonisjs/core/http';
-import  { type I18n } from '@adonisjs/i18n';
+import { type I18n } from '@adonisjs/i18n';
 
 export class SimpleAuthException extends Exception {
   public static readonly code: 'E_SIMPLE_UNAUTHORIZED_ACCESS';
